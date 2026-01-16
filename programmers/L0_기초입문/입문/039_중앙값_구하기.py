b08f22eb77b0f548b8294aa5b7616c81840059dd
sybeom528 <sybeom528@gmail.com>
2026-01-16 17:52:37 +0900

    [프로그래머스] L0 - 중앙값 구하기 (#39)

--- programmers/L0_기초입문/입문/039_중앙값_구하기.py
+++ programmers/L0_기초입문/입문/039_중앙값_구하기.py
@@ -3,8 +3,13 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120811
 # 알고리즘: 기초
 # 작성자: 서윤범
-# 작성일: 2026. 01. 16. 17:51:17
+# 작성일: 2026. 01. 16. 17:52:34
+
+# def solution(array):
+#     array = sorted(array)
+#     return array[len(array) // 2]
+
+import statistics
 
 def solution(array):
-    array = sorted(array)
-    return array[len(array) // 2]
+    return statistics.median(array)
