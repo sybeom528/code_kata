631da1bd0c5110d1b144a20be4cb12787dc79b3f
sybeom528 <sybeom528@gmail.com>
2026-01-16 12:25:37 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (1) (#16)

---
+++ programmers/L0_기초입문/입문/016_피자_나눠_먹기__1_.py
@@ -0,0 +1,10 @@
+# 피자 나눠 먹기 (1)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120814
+# 알고리즘: 기초
+# 작성자: 서윤범
+# 작성일: 2026. 01. 16. 12:25:34
+
+import math
+def solution(n):
+    return math.ceil(n / 7)
