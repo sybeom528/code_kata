4fb2a4098f2bd8e87799baa965a7b3f58087c2e2
sybeom528 <sybeom528@gmail.com>
2026-01-16 13:21:21 +0900

    [프로그래머스] L0 - 아이스 아메리카노 (#28)

---
+++ programmers/L0_기초입문/입문/028_아이스_아메리카노.py
@@ -0,0 +1,9 @@
+# 아이스 아메리카노
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120819
+# 알고리즘: 기초
+# 작성자: 서윤범
+# 작성일: 2026. 01. 16. 13:21:19
+
+def solution(money):
+    return [money // 5500, money % 5500]
