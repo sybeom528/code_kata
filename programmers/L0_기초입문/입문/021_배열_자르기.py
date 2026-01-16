53e9da6b52f423e81785c637ae4132683562d2ea
sybeom528 <sybeom528@gmail.com>
2026-01-16 12:52:19 +0900

    [프로그래머스] L0 - 배열 자르기 (#21)

---
+++ programmers/L0_기초입문/입문/021_배열_자르기.py
@@ -0,0 +1,9 @@
+# 배열 자르기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120833
+# 알고리즘: 기초
+# 작성자: 서윤범
+# 작성일: 2026. 01. 16. 12:52:17
+
+def solution(numbers, num1, num2):
+    return numbers[num1:num2 + 1]
