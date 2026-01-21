bd4b1cdcb751278a0108545c4df4ce126dec2ee0
sybeom528 <sybeom528@gmail.com>
2026-01-21 10:19:54 +0900

    [프로그래머스] L0 - 7의 개수 (#67)

--- programmers/L0_기초입문/입문/067_7의_개수.py
+++ programmers/L0_기초입문/입문/067_7의_개수.py
@@ -3,9 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120912
 # 알고리즘: 기초
 # 작성자: 서윤범
-# 작성일: 2026. 01. 21. 10:19:42
+# 작성일: 2026. 01. 21. 10:19:53
 
 def solution(array):
-    
-    answer = sum([str(num).count('7') for num in array])
-    return answer
+    return sum([str(num).count('7') for num in array])
