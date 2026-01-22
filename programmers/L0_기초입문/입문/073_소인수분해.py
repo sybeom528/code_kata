0755c60f16ce7a10bccd36790a2ddf50f6a650b4
sybeom528 <sybeom528@gmail.com>
2026-01-22 11:07:47 +0900

    [프로그래머스] L0 - 소인수분해 (#73)

--- programmers/L0_기초입문/입문/073_소인수분해.py
+++ programmers/L0_기초입문/입문/073_소인수분해.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120852
 # 알고리즘: 기초
 # 작성자: 서윤범
-# 작성일: 2026. 01. 22. 11:06:09
+# 작성일: 2026. 01. 22. 11:07:46
 
 import math
 
@@ -11,11 +11,14 @@ def solution(n):
     answer = []
     mod = 2
     
-    while n >= mod:
+    while n >= mod ** 2:
         if n % mod == 0:
             answer.append(mod)
             while n % mod == 0:
                 n = n // mod
         mod += 1
+    
+    if n > 1:
+        answer.append(n)
     
     return answer
