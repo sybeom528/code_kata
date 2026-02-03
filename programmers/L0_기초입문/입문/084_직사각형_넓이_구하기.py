75fa9a9a8d7627d35b08a128dab7b08c8f3a54e4
sybeom528 <sybeom528@gmail.com>
2026-02-03 19:18:17 +0900

    [프로그래머스] L0 - 직사각형 넓이 구하기 (#84)

---
+++ programmers/L0_기초입문/입문/084_직사각형_넓이_구하기.py
@@ -0,0 +1,13 @@
+# 직사각형 넓이 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120860
+# 알고리즘: 기초
+# 작성자: 서윤범
+# 작성일: 2026. 02. 03. 19:18:15
+
+def solution(dots):
+    min_x = min([d[0] for d in dots])
+    max_x = max([d[0] for d in dots])
+    min_y = min([d[1] for d in dots])
+    max_y = max([d[1] for d in dots])
+    return (max_x - min_x) * (max_y - min_y)
