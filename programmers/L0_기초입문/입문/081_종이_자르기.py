e760155c0eab3b893675f2c59fdf8626a0cf2df3
sybeom528 <sybeom528@gmail.com>
2026-02-03 16:38:12 +0900

    [프로그래머스] L0 - 종이 자르기 (#81)

---
+++ programmers/L0_기초입문/입문/081_종이_자르기.py
@@ -0,0 +1,9 @@
+# 종이 자르기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120922
+# 알고리즘: 기초
+# 작성자: 서윤범
+# 작성일: 2026. 02. 03. 16:38:11
+
+def solution(M, N):
+    return M * N - 1
