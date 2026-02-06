29390700bf112afe8328a8ee1ba91516618243c6
sybeom528 <sybeom528@gmail.com>
2026-02-06 13:50:38 +0900

    [프로그래머스] L0 - 문자열 곱하기 (#110)

---
+++ programmers/L0_기초입문/기초트레이닝/110_문자열_곱하기.py
@@ -0,0 +1,9 @@
+# 문자열 곱하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/181940
+# 알고리즘: 연산
+# 작성자: 서윤범
+# 작성일: 2026. 02. 06. 13:50:37
+
+def solution(my_string, k):
+    return my_string * k
