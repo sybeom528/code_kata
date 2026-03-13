43f09bc19e1d01960ad6d66d4e717f5b6e39ce20
sybeom528 <sybeom528@gmail.com>
2026-03-13 17:57:41 +0900

    [HackerRank] 고급 - Ollivander's Inventory (#173)

---
+++ hackerrank/4_고급/173_Ollivander's_Inventory.sql
@@ -0,0 +1,7 @@
+-- Ollivander's Inventory
+-- HackerRank 고급 (⭐⭐⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/harry-potter-and-wands/problem
+-- 작성자: 서윤범
+-- 작성일: 2026. 03. 13. 17:57:40
+
+ORDER BY W.POWER DESC, P.AGE DESC
