89a337a4d31b853aba9106d79ec64bc98b212b87
sybeom528 <sybeom528@gmail.com>
2026-03-13 11:30:17 +0900

    [HackerRank] 중급 - Weather Observation Station 5 (#137)

---
+++ hackerrank/3_중급/137_Weather_Observation_Station_5.sql
@@ -0,0 +1,7 @@
+-- Weather Observation Station 5
+-- HackerRank 중급 (⭐⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+-- 작성자: 서윤범
+-- 작성일: 2026. 03. 13. 11:30:17
+
+ORDER BY LENGTH(CITY)
