d25109b9431bb6290cca215e299d7e9952e52b98
sybeom528 <sybeom528@gmail.com>
2026-07-15 21:05:39 +0900

    [프로그래머스] L4 - 베스트앨범 (#359)

--- programmers/L4_중상/359_베스트앨범.py
+++ programmers/L4_중상/359_베스트앨범.py
@@ -3,25 +3,47 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42579
 # 알고리즘: 해시, 정렬
 # 작성자: 서윤범
-# 작성일: 2026. 07. 15. 20:43:03
+# 작성일: 2026. 07. 15. 21:05:36
+
+# AI 풀이
+# 애초에 두 개의 다른 딕셔너리를 만들어서 sorted하는 구조
+from collections import defaultdict
 
 def solution(genres, plays):
+    total = defaultdict(int)
+    songs = defaultdict(list)
     answer = []
     
-    genre_dict = {}
-    
-    for i in range(len(genres)):
-        if genres[i] not in genre_dict:
-            genre_dict[genres[i]] = [plays[i], [[plays[i], i]]]
-        else:
-            genre_dict[genres[i]][0] += plays[i]
-            genre_dict[genres[i]][1].append([plays[i],i])
-            genre_dict[genres[i]][1].sort(key = lambda x: (-x[0], x[1]))
-    
-    genre_sorted = sorted(genre_dict.items(), key = lambda x: (-x[1][0]))
-    
-    for i in range(len(genre_sorted)):
-        for j in range(min(2, len(genre_sorted[i][1][1]))):
-            answer.append(genre_sorted[i][1][1][j][1])
+    for i, (g,p) in enumerate(zip(genres, plays)):
+        total[g] += p
+        songs[g].append([p,i])
+        
+    for g in sorted(total, key = lambda x: -total[x]):
+        top = sorted(songs[g], key = lambda x: (-x[0], x[1]))
+        answer += [i for p, i in top[:2]]
     
     return answer
+
+
+
+# 내 풀이
+# def solution(genres, plays):
+#     answer = []
+    
+#     genre_dict = {}
+    
+#     for i in range(len(genres)):
+#         if genres[i] not in genre_dict:
+#             genre_dict[genres[i]] = [plays[i], [[plays[i], i]]]
+#         else:
+#             genre_dict[genres[i]][0] += plays[i]
+#             genre_dict[genres[i]][1].append([plays[i],i])
+#             genre_dict[genres[i]][1].sort(key = lambda x: (-x[0], x[1]))
+    
+#     genre_sorted = sorted(genre_dict.items(), key = lambda x: (-x[1][0]))
+    
+#     for i in range(len(genre_sorted)):
+#         for j in range(min(2, len(genre_sorted[i][1][1]))):
+#             answer.append(genre_sorted[i][1][1][j][1])
+    
+#     return answer
