05bb59b186aa94119678b54613b206539932a309
sybeom528 <sybeom528@gmail.com>
2026-07-18 16:18:25 +0900

    [프로그래머스] 게임 맵 최단거리 풀이 제출
    - 문제 ID: 1844
    - 난이도: unknown
    - 제출일: 2026. 07. 18. 16:18:19
    - 작성자: 서윤범

---
+++ programmers/other/1844_게임_맵_최단거리.py
@@ -0,0 +1,31 @@
+# 게임 맵 최단거리
+# 프로그래머스 (unknown)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/1844
+# 작성자: 서윤범
+# 작성일: 2026. 07. 18. 16:18:19
+
+from collections import deque
+
+def solution(maps):
+    
+    n = len(maps); m = len(maps[0])
+    if maps[n-1][m-2] == 0 and maps[n-2][m-1] == 0:
+        return -1
+    
+    queue = deque()
+    maps[0][0] = 1
+    queue.append((0,0))
+    
+    dx = [1,0,-1,0]
+    dy = [0,1,0,-1]
+    
+    while queue:
+        x,y = queue.popleft()
+        if x == n - 1 and y == m - 1:
+            return maps[x][y]
+        for i in range(4):
+            next_x = x + dx[i]
+            next_y = y + dy[i]
+            if 0 <= next_x < n and 0 <= next_y < m and maps[next_x][next_y] == 1:
+                maps[next_x][next_y] = maps[x][y] + 1
+                queue.append((next_x, next_y))
