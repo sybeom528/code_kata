9d62fdeb745d4e54395e32cce38b0d3a7f46d832
sybeom528 <sybeom528@gmail.com>
2026-07-20 17:25:41 +0900

    [프로그래머스] 레벨7 - N으로 표현 (#377)

---
+++ programmers/L7_챌린지/377_N으로_표현.py
@@ -0,0 +1,33 @@
+# N으로 표현
+# 프로그래머스 레벨7 (챌린지)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42895
+# 알고리즘: DP
+# 작성자: 서윤범
+# 작성일: 2026. 07. 20. 17:25:40
+
+def solution(N, number):
+    
+    DP = [[] for _ in range(9)]
+    
+    DP[1] = [N]
+    
+    if number == DP[1][0]:
+        return 1
+    
+    for i in range(2, 9):
+        for j in range(1,i):
+            for num1 in DP[j]:
+                for num2 in DP[i-j]:
+                    plus = num1 + num2
+                    minus = num1 - num2
+                    prod = num1 * num2
+                    if num2 != 0:
+                        devide = num1 // num2
+                    temp = [plus, minus, prod, devide]
+                    DP[i].extend(temp)
+        DP[i] = list(set(DP[i]))
+        DP[i].append(int(str(N) * i))
+        if number in DP[i]:
+            return i
+
+    return -1
