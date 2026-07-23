69d8c8ddb3b331595a89d76bb97c4847aa9fb03d
sybeom528 <sybeom528@gmail.com>
2026-07-23 17:19:43 +0900

    [프로그래머스] L4 - 가장 큰 수 (#316)

--- programmers/L4_중상/316_가장_큰_수.py
+++ programmers/L4_중상/316_가장_큰_수.py
@@ -3,19 +3,51 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42746
 # 알고리즘: 정렬
 # 작성자: 서윤범
-# 작성일: 2026. 07. 16. 15:26:22
+# 작성일: 2026. 07. 23. 17:19:41
 
 from functools import cmp_to_key
 
-def cmp(a, b):
-    if a + b > b + a: return -1
-    elif a + b < b + a: return 1
-    else: return 0
-
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=cmp_to_key(cmp))
-    if numbers[0] == '0':
+    
+    def sort_numbers(a,b):
+        if a + b > b + a:
+            return -1
+        elif a + b < b + a:
+            return 1
+        else:
+            return 0
+        
+    str_numbers = [str(num) for num in numbers]
+    str_numbers.sort(key = cmp_to_key(sort_numbers))
+    
+    if str_numbers[0] == '0':
         return '0'
     else:
-        return ''.join(numbers)
+        return ''.join(str_numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from functools import cmp_to_key
+
+# def cmp(a, b):
+#     if a + b > b + a: return -1
+#     elif a + b < b + a: return 1
+#     else: return 0
+
+# def solution(numbers):
+#     numbers = list(map(str, numbers))
+#     numbers.sort(key=cmp_to_key(cmp))
+#     if numbers[0] == '0':
+#         return '0'
+#     else:
+#         return ''.join(numbers)
