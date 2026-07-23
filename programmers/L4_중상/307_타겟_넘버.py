6cdb491483267a95232bb680bb81b2a399792b3d
sybeom528 <sybeom528@gmail.com>
2026-07-23 17:50:50 +0900

    [프로그래머스] L4 - 타겟 넘버 (#307)

--- programmers/L4_중상/307_타겟_넘버.py
+++ programmers/L4_중상/307_타겟_넘버.py
@@ -3,7 +3,45 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/43165
 # 알고리즘: DFS, 완전탐색
 # 작성자: 서윤범
-# 작성일: 2026. 07. 18. 14:37:20
+# 작성일: 2026. 07. 23. 17:50:48
+
+from collections import deque
+
+def solution(numbers, target):
+    answer = 0
+    n = len(numbers)
+    
+    queue = deque()
+    queue.append((-1,0))
+    
+    while queue:
+        idx, num = queue.popleft()
+        if idx + 1 < n:
+            queue.append((idx + 1, num + numbers[idx]))
+            queue.append((idx + 1, num - numbers[idx]))
+        else:
+            if num == target:
+                answer += 1
+    
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # def solution(numbers, target):
 #     answer = 0
@@ -23,24 +61,24 @@
     
 #     return DFS(0,0)
 
-from collections import deque
+# from collections import deque
 
-def solution(numbers, target):
-    answer = 0
-    queue = deque()
-    n = len(numbers)
+# def solution(numbers, target):
+#     answer = 0
+#     queue = deque()
+#     n = len(numbers)
     
-    queue.append([0, numbers[0]])
-    queue.append([0, -numbers[0]])
+#     queue.append([0, numbers[0]])
+#     queue.append([0, -numbers[0]])
     
-    while queue:
-        idx, total = queue.popleft()
-        if idx == n - 1:
-            if total == target:
-                answer += 1
-        else:
-            queue.append([idx + 1, total + numbers[idx + 1]])    
-            queue.append([idx + 1, total - numbers[idx + 1]])    
+#     while queue:
+#         idx, total = queue.popleft()
+#         if idx == n - 1:
+#             if total == target:
+#                 answer += 1
+#         else:
+#             queue.append([idx + 1, total + numbers[idx + 1]])    
+#             queue.append([idx + 1, total - numbers[idx + 1]])    
         
     
-    return answer
+#     return answer
