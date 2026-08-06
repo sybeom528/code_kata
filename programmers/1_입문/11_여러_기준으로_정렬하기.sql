75fb9b04a469834b8e62e729af2b9ec008aa887f
sybeom528 <sybeom528@gmail.com>
2026-08-06 16:43:24 +0900

    [프로그래머스] 입문 - 여러 기준으로 정렬하기 (#11)

---
+++ programmers/1_입문/11_여러_기준으로_정렬하기.sql
@@ -0,0 +1,9 @@
+-- 여러 기준으로 정렬하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59404
+-- 작성자: 서윤범
+-- 작성일: 2026. 08. 06. 16:43:19
+
+SELECT ANIMAL_ID, NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME, DATETIME DESC
