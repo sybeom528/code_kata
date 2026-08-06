8882222df96f86520f45b42dc1624168865e5dd7
sybeom528 <sybeom528@gmail.com>
2026-08-06 15:49:10 +0900

    [프로그래머스] 입문 - 이름이 없는 동물의 아이디 (#21)

---
+++ programmers/1_입문/21_이름이_없는_동물의_아이디.sql
@@ -0,0 +1,8 @@
+-- 이름이 없는 동물의 아이디
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59039
+-- 작성자: 서윤범
+-- 작성일: 2026. 08. 06. 15:49:07
+
+SELECT ANIMAL_ID FROM ANIMAL_INS
+WHERE NAME IS NULL
