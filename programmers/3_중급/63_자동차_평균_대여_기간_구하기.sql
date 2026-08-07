e812ab8d00a74ff1ea9f15c9da3d276a1bf1b747
sybeom528 <sybeom528@gmail.com>
2026-08-07 16:53:23 +0900

    [프로그래머스] 중급 - 자동차 평균 대여 기간 구하기 (#63)

---
+++ programmers/3_중급/63_자동차_평균_대여_기간_구하기.sql
@@ -0,0 +1,11 @@
+-- 자동차 평균 대여 기간 구하기
+-- 프로그래머스 중급 (⭐⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/157342
+-- 작성자: 서윤범
+-- 작성일: 2026. 08. 07. 16:53:19
+
+SELECT CAR_ID, ROUND(AVG(DATEDIFF(END_DATE, START_DATE) + 1),1) AS AVERAGE_DURATION
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+HAVING AVERAGE_DURATION >= 7
+ORDER BY AVERAGE_DURATION DESC, CAR_ID DESC
