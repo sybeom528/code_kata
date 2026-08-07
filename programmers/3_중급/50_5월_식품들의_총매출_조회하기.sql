98a380eef4d0bea44d528c63a57799153aa4de9d
sybeom528 <sybeom528@gmail.com>
2026-08-07 15:09:50 +0900

    [프로그래머스] 중급 - 5월 식품들의 총매출 조회하기 (#50)

---
+++ programmers/3_중급/50_5월_식품들의_총매출_조회하기.sql
@@ -0,0 +1,13 @@
+-- 5월 식품들의 총매출 조회하기
+-- 프로그래머스 중급 (⭐⭐⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131117
+-- 작성자: 서윤범
+-- 작성일: 2026. 08. 07. 15:09:46
+
+SELECT P.PRODUCT_ID, P.PRODUCT_NAME, SUM(P.PRICE * O.AMOUNT) AS TOTAL_SALES 
+FROM FOOD_ORDER AS O
+JOIN FOOD_PRODUCT AS P
+ON O.PRODUCT_ID = P.PRODUCT_ID
+WHERE DATE_FORMAT(O.PRODUCE_DATE, '%Y-%m') = '2022-05'
+GROUP BY P.PRODUCT_ID
+ORDER BY TOTAL_SALES DESC, PRODUCT_ID
