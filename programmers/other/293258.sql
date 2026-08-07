e734a6ac57696bb9da4be1b87b60cf8d188a807d
sybeom528 <sybeom528@gmail.com>
2026-08-07 15:30:47 +0900

    [프로그래머스] other - 잔챙이 잡은 수 구하기

--- programmers/other/293258.sql
+++ programmers/other/293258.sql
@@ -2,7 +2,8 @@
 -- 프로그래머스 (미등록 문제)
 -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/293258
 -- 작성자: 서윤범
--- 작성일: 2026. 06. 16. 11:02:07
+-- 작성일: 2026. 08. 07. 15:30:43
 
-select count(*) as fish_count from fish_info
-where isnull(length)
+SELECT COUNT(*) AS FISH_COUNT
+FROM FISH_INFO
+WHERE LENGTH IS NULL
