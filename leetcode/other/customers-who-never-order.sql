029c44ccfc5d7072d6a08fbab109391de8a4e3b5
sybeom528 <sybeom528@gmail.com>
2026-08-09 16:26:27 +0900

    [LeetCode] other - 183. Customers Who Never Order

--- leetcode/other/customers-who-never-order.sql
+++ leetcode/other/customers-who-never-order.sql
@@ -2,10 +2,10 @@
 -- LeetCode (미등록 문제)
 -- 문제 링크: https://leetcode.com/problems/customers-who-never-order/
 -- 작성자: 서윤범
--- 작성일: 2026. 08. 09. 16:26:13
+-- 작성일: 2026. 08. 09. 16:26:26
 
 # Write your MySQL query statement below
-select distinct c.name as Customers from customers as c
+select c.name as Customers from customers as c
 left join orders as o
 on c.id = o.customerId
 where o.id is null
